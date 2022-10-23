138b60335091131e205c04982966d47f39e7fdb6
Artem Vasylenko <avasylenko@revenuewell.com>
2022-10-23 20:41:46 +0300

    Move Multiplatform Settings separately for each platform

--- shared/src/iosMain/kotlin/com/paypay/currency_converter/di/Module.kt
+++ shared/src/iosMain/kotlin/com/paypay/currency_converter/di/Module.kt
@@ -1,6 +1,7 @@
 package com.paypay.currency_converter.di
 
 import platform.Foundation.NSLocale
+import platform.Foundation.NSUserDefaults
 import platform.Foundation.currentLocale
 import platform.Foundation.languageCode
 
@@ -9,6 +10,8 @@ import org.koin.dsl.module
 
 import io.ktor.client.engine.darwin.*
 
+import com.russhwolf.settings.NSUserDefaultsSettings
+
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
@@ -16,6 +19,9 @@ import com.paypay.currency_converter.db.CurrencyDatabase
 import com.paypay.currency_converter.utils.Language
 
 actual fun platformModule(): Module = module {
+    single {
+        NSUserDefaultsSettings(NSUserDefaults.standardUserDefaults)
+    }
     factory<Language> { NSLocale.currentLocale.languageCode }
     single<SqlDriver> { NativeSqliteDriver(CurrencyDatabase.Schema, "currency.db") }
     single { Darwin.create() }
