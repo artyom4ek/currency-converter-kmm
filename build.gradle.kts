62cabb379206519b070c4c0e702e7755187b8e98
Artem Vasylenko <avasylenko@revenuewell.com>
2022-10-24 11:39:35 +0300

    Fix Gradle duplicate handling strategy error

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,15 @@ allprojects {
     }
 }
 
+/* https://github.com/gradle/gradle/issues/17236 */
+gradle.taskGraph.whenReady {
+    allTasks
+        .filter { it.hasProperty("duplicatesStrategy") }
+        .forEach {
+            it.setProperty("duplicatesStrategy", "EXCLUDE")
+        }
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
